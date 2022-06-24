6654a665efb4f90050058f95dc5c99c5744b7638
BrunoAraus <101360453+BrunoAraus@users.noreply.github.com>
2022-06-23 21:11:05 -0400

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('', home,name="home"),
+    path('usuario', usuario,name="usuario"),
+    path('historial', historial,name="historial"),
+    path('pedido', pedido,name="pedido"),
+    path('CrearPlato', CrearPlato, name="CrearPlato"),
+    path('Gestionar', Gestionar, name="Gestionar"),
+    path('eliminarPlato/<id>', eliminarPlato, name="eliminarPlato"),
+    path('modificarPlato/<id>', modificarPlato, name="modificarPlato"),
+    path('producto/<id>', producto, name="producto"),
+    path('login', LoginView.as_view(template_name='core/login.html'), name="login"),
+    path('logout', LogoutView.as_view(template_name='core/logout.html'), name="logout"),
+    path('registro', registro,name="registro"),
+]
---
+++ core/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+from core.forms import PlatoForm
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+
+
+def registro(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to="login")
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/registro.html',{'form':form})
+
+
+
+
+def home(request):
+    contexto = {'plato': Plato.objects.all()}
+    return render(request, 'core/index.html',contexto)
+
+def Gestionar(request):
+    contexto = {'plato': Plato.objects.all()}
+    return render(request, 'core/Gestionar.html',contexto)
+
+def usuario(request):
+    return render(request, 'core/usuario.html')
+
+def historial(request):
+    return render(request, 'core/historial.html')
+
+def pedido(request):
+    return render(request, 'core/pedido.html')
+
+
+def CrearPlato(request):
+    contexto = {'form': PlatoForm()}
+    if request.method == "POST":
+        Plato = PlatoForm(request.POST)
+        if Plato.is_valid:
+            Plato.save()
+            contexto["mensaje"] = "Plato agregado!."
+    return render(request, 'core/CrearPlato.html', contexto)
+
+def modificarPlato(request, id):
+    plato = Plato.objects.get(nombre=id)
+    contexto = {'form': PlatoForm(instance=plato)}
+    if request.method == "POST":
+        plato = PlatoForm(request.POST, instance=plato)
+        if plato.is_valid:
+            plato.save()
+            contexto["mensaje"] = "plato modificado!."
+            contexto['form'] = plato
+    return render(request, 'core/modificarPlato.html', contexto)
+
+def eliminarPlato(request, id):
+    plato = Plato.objects.get(nombre=id)
+    plato.delete()
+    return redirect(to="Gestionar")
+
+
+def producto(request, id):
+    plato = Plato.objects.get(nombre=id)
+    contexto = {'plato': plato}
+    return render(request, 'core/producto.html', contexto)
