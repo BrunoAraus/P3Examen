92a1ccb1f0dffc0e7a33cfe5063721b0e96bb7c3
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2022-07-06 23:21:17 -0400

    Intento fallido
    no logro solucionar los errores, pero igual dejé comentados los codigos de como lo estaba implementando

--- carrito/urls.py
+++ carrito/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path
 
 import carrito
+from core.views import agregar_prducto, eliminar_producto, limpiar_carrito, restar_producto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', carrito, name="Carrito")
+    path('', carrito, name="Carrito"),
+    path('agregar/<int:producto_id>/', agregar_prducto, name="Add"),
+    path('eliminar/<int:producto_id>/', eliminar_producto, name="Del"),
+    path('restar/<int:producto_id>/', restar_producto, name="Sub"),
+    path('limpiar/', limpiar_carrito, name="CLS"),
+    
 ]
