65d46e7d440c5275e5e55158cce35abec1a60164
BrunoAraus <101360453+BrunoAraus@users.noreply.github.com>
2022-10-05 18:14:23 -0300

    Sqllite añadido

--- TestDjango/settings.py
+++ TestDjango/settings.py
@@ -83,10 +83,8 @@ WSGI_APPLICATION = 'TestDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'localhost:1521/xe',
-        'USER':'django',
-        'PASSWORD': 'django',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
